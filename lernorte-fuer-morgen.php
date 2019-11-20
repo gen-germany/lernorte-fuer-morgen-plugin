4bae55b3f3e6cc3f11a4b27d183c98865d05d6e9
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2019-11-20 08:52:46 +0100

    whitespace and doc fixes

--- lernorte-fuer-morgen.php
+++ lernorte-fuer-morgen.php
@@ -38,26 +38,36 @@ if(!defined('LFM_PLUGIN_DIR')) {
 /** Custom Dashboard Widget */
 add_action('wp_dashboard_setup', 'lfm_dashboard_widgets');
 
+/** Callback to add dashboard widget and meta boxes */
 function lfm_dashboard_widgets() {
   wp_add_dashboard_widget('lfm_help_widget', __('Lernorte für Morgen'), 'custom_dashboard_help');
   // The other widgets should be at the side, so use add_meta_box instead
-  add_meta_box('lfm_dashboard_veranstaltungen', __('Lernorte für Morgen - Veranstaltungen'), 'lfm_dashboard_veranstaltungen', 'dashboard', 'side', 'high');
-  add_meta_box('lfm_dashboard_referentn', __('Lernorte für Morgen - Referent*n'), 'lfm_dashboard_referentn', 'dashboard', 'side', 'high');
+  add_meta_box('lfm_dashboard_veranstaltungen',
+    __('Lernorte für Morgen - Veranstaltungen'),
+    'lfm_dashboard_veranstaltungen',
+    'dashboard', 'side', 'high');
+  add_meta_box('lfm_dashboard_referentn',
+    __('Lernorte für Morgen - Referent*n'),
+    'lfm_dashboard_referentn', 'dashboard', 'side', 'high');
 }
 
+/** Callback for the dashboard referentn metabox */
 function lfm_dashboard_referentn() {
   require_once( LFM_PLUGIN_DIR . '/dashboard/referentn_widget.php' );
 }
 
+/** Callback for the dashboard veranstaltungen metabox */
 function lfm_dashboard_veranstaltungen() {
   require_once( LFM_PLUGIN_DIR . '/dashboard/veranstaltungen_widget.php' );
 }
 
+/** Callback for the dashboard help widget */
 function custom_dashboard_help() {
   require_once( LFM_PLUGIN_DIR . '/dashboard/help_widget.php' );
 }
 
-/* Remove unwanted dashboard widgets. Names correspond to what can be found using Inspect Element (id of widget-element). */
+/** Callback to remove unwanted dashboard widgets.
+  * Names correspond to what can be found using Inspect Element (id of widget-element). */
 function remove_dashboard_widgets() {
  // Remove Welcome to WordPress! widget
  // https://codex.wordpress.org/Plugin_API/Action_Reference/welcome_panel
@@ -76,6 +86,7 @@ function remove_dashboard_widgets() {
  remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
 }
 
+/** Remove default dashboard widgets */
 add_action( 'wp_dashboard_setup', 'remove_dashboard_widgets' );
 
 /** Modify Welcome Dashboard Panel */
@@ -83,6 +94,7 @@ function lfm_dashboard_welcome_panel() {
   require_once( LFM_PLUGIN_DIR . '/dashboard/welcome_panel.php' );
 }
 
+/** Replace dashboard welcome panel */
 add_action( 'welcome_panel', 'lfm_dashboard_welcome_panel' );
 
 
@@ -90,7 +102,6 @@ add_action( 'welcome_panel', 'lfm_dashboard_welcome_panel' );
 define('PODS_SHORTCODE_ALLOW_SUB_SHORTCODES',true);
 /**
 * Allow Pods Templates to use shortcodes
-*
 * NOTE: Will only work if the constant PODS_SHORTCODE_ALLOW_SUB_SHORTCODES is true.
 */
 add_filter( 'pods_shortcode', function( $tags )  {
@@ -99,10 +110,11 @@ add_filter( 'pods_shortcode', function( $tags )  {
   return $tags;
 });
 
-// Update CSS within in Admin
+/* Callback to update CSS within in Admin */
 function admin_style() {
   wp_enqueue_style('admin-styles', plugins_url('/admin.css', __FILE__));
 }
+/* Update CSS within Admin backend */
 add_action('admin_enqueue_scripts', 'admin_style');
 
 ?>
