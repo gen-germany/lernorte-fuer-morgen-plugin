f8dba522a2784387c0045dc6aa079e09b2dd8c69
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2019-12-11 20:46:50 +0100

    only restrict non-administrators to their own media

--- restrict-media/restrict-media.php
+++ restrict-media/restrict-media.php
@@ -18,10 +18,15 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Donate link: http://www.jamviet.com/2015/05/restrict-author-posting.html
 */
 
-/* Restrict User using own Media */
+/* Restrict User using own Media except for admin */
 function jamviet_restrict_author_file_in_media( $wp_query = array() ) {
-  $user_id = get_current_user_id();
-  if ( get_user_meta( $user_id , '_restrict_media', true) ) {
+  $user = wp_get_current_user();
+  if ( in_array( 'administrator', (array) $user->roles ) ) {
+    // The user has the "administrator" role, can see all media.
+  }
+  else {
+    // This user shall see its own media items only.
+    $user_id = get_current_user_id();
     $wp_query['author'] =  $user_id;
   }
   return $wp_query;
