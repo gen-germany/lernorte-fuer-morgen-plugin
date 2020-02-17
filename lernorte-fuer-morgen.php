fb7ae67976743b054cd46fbae572be6e28f8b104
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-02-17 20:05:52 +0100

    Remove divi backend "projects" stuff.

--- lernorte-fuer-morgen.php
+++ lernorte-fuer-morgen.php
@@ -148,4 +148,18 @@ function lfm_autoselect_bildungsanbieter($pieces, $is_new_item) {
   return $pieces;
 }
 
+/**
+ * Remove the Divi "Project" stuff from backend.
+ */
+add_filter( 'et_project_posttype_args', 'lfm_et_project_posttype_args', 10, 1 );
+function lfm_et_project_posttype_args( $args ) {
+	return array_merge( $args, array(
+		'public'              => false,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => false,
+		'show_in_nav_menus'   => false,
+		'show_ui'             => false
+	));
+}
+
 ?>
