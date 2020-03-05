d86097f79446a621cfa7f16b3ee2f2402eed4b6b
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-03-05 14:06:40 +0100

    overwrite translations for 'more fields', too

--- admin/excerpt_labels.php
+++ admin/excerpt_labels.php
@@ -1,9 +1,13 @@
 <?
 
+/** Pretty hardcore, overwrite gettext translation function. */
 function lfm_excerpt_label( $translation, $original ) {
     if ( false !== strpos( $original, 'Excerpts are optional hand-crafted summaries of your' ) ) {
         return __( 'Textauszüge sind optionale, von Hand erstellte kurze Zusammenfassungen, die auf der Webseite als "Teasertext" angezeigt werden.' );
+    } elseif ( "More Fields" == $original ) {
+      return 'Weitere Angaben';
     }
+
     return $translation;
 }
 add_filter( 'gettext', 'lfm_excerpt_label', 10, 2 );
