be1e58d733731c3ed7563c3679b90e10fd7bc143
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-03-09 09:22:35 +0100

    s/bildungsanbieter/lernort/

--- pods/lernorte_save.php
+++ pods/lernorte_save.php
@@ -1,29 +1,29 @@
 <?php
 
 /* Auto-select first Bildungsanbieter when not set and saving a Veranstaltung (pre-save). */
-add_filter('pods_api_pre_save_pod_item_veranstaltung', 'lfm_autoselect_bildungsanbieter', 10, 2);
+add_filter('pods_api_pre_save_pod_item_veranstaltung', 'lfm_autoselect_lernort', 10, 2);
 
-function lfm_autoselect_bildungsanbieter($pieces, $is_new_item) {
+function lfm_autoselect_lernort($pieces, $is_new_item) {
   // PHP special: '' == NULL (but not ===)
-  if ($pieces[ 'fields' ][ 'bildungsanbieter' ][ 'value' ] == NULL) {
+  if ($pieces[ 'fields' ][ 'lernort' ][ 'value' ] == NULL) {
     // Mark field as 'dirty', to process changes.
-    if ( ! isset( $pieces[ 'fields_active' ][ 'bildungsanbieter' ] ) ) {
-	  	array_push ($pieces[ 'fields_active' ], 'bildungsanbieter' );
+    if ( ! isset( $pieces[ 'fields_active' ][ 'lernort' ] ) ) {
+	     array_push ($pieces[ 'fields_active' ], 'lernort' );
     }
-    // Find relevant bildungsanbieter
+    // Find relevant lernort
     $params = array(
       'where' => 'post_author = ' . get_current_user_id()
     );
 
-    $bildungsanbieter = pods( 'bildungsanbieter', $params );
-    $bildungsanbieter_data = $bildungsanbieter->data();
+    $lernort = pods( 'lernort', $params );
+    $lernort_data = $lernort->data();
 
-    if ($bildungsanbieter_data == NULL) {
+    if ($lernort_data == NULL) {
       return pods_error("Bitte lege zuerst einen Bildungsanbieter an.");
     }
 
-    $first_bildungsanbieter_id = reset($bildungsanbieter_data)->ID;
-    $pieces[ 'fields' ][ 'bildungsanbieter' ][ 'value' ] = $first_bildungsanbieter_id;
+    $first_lernort_id = reset($lernort_data)->ID;
+    $pieces[ 'fields' ][ 'lernort' ][ 'value' ] = $first_lernort_id;
   }
 
   return $pieces;
