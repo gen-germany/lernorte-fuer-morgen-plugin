69a64210a93b5405dec4cd01563c059f158c3fbb
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-05-13 15:21:31 +0200

    refactor: extract menu item creation for calendar year

--- nav/auto_menu.php
+++ nav/auto_menu.php
@@ -7,6 +7,53 @@ function lfm_add_calendar_menu_items ( $items, $args ) {
   return $items;
 }*/
 
+/** Adds navigation ("menu") items under $cal_nav_pseudo_id.
+ * The first added item (with $year_nav_pseudo_id) is the year, under this one
+ * item for every month till the end of the year (starting at $start_date) */
+function lfm_add_year_month_items( &$items, $start_date, $year_nav_pseudo_id, $cal_nav_pseudo_id ) {
+  $current_year = $start_date->format( 'Y' );
+
+  $items[] = (object) array(
+    'title' => $current_year,
+    'ID' => 'nav_current_year',
+    'menu_item_parent' => $cal_nav_pseudo_id,
+    'db_id' => $year_nav_pseudo_id,
+    'current' => false,
+    'xfn'     => '',
+    'target'  => '',
+    'classes' => array( 'menu-item', 'menu-item-has-children' ),
+    'url' => site_url( '/veranstaltungen/' . $current_year )
+  );
+
+  /*
+  $items[] = (object) array(
+    'title' => $today_dt->format( 'F' ),
+    'ID' => 'nav_current_month',
+    'menu_item_parent' => $current_walk_id,
+    'db_id'   => $current_walk_id += 1,
+    'current' => false,
+    'target'  => '',
+    'url' => 'https://bog.us/me/too'
+  );
+   */
+
+  $current_pseudo_id = $year_nav_pseudo_id;
+  while ( $start_date->format( 'Y' ) == $current_year ) {
+    $items[] = (object) array(
+      'title' => date_i18n( 'F', $start_date->getTimestamp()),
+      'ID' => 'nav_current_month',
+      'menu_item_parent' => $year_nav_pseudo_id,
+      'db_id'   => $current_pseudo_id += 1,
+      'current' => false,
+      'xfn'     => '',
+      'target'  => '',
+      'url'     => site_url( '/veranstaltungen/' . $current_year . '/' . $start_date->format( 'm' ) )
+    );
+    $start_date->modify( '+1 month' );
+  }
+  return $items;
+}
+
 function lfm_add_calendar_items ( $items, $args) {
   // Only attach to main menu
   if ( 'primary-menu' !== $args->theme_location ) {
@@ -40,44 +87,14 @@ function lfm_add_calendar_items ( $items, $args) {
     'url' => site_url( '/veranstaltungen/' . $current_year )
   );
 
-  $items[] = (object) array(
-    'title' => $current_year,
-    'ID' => 'nav_current_year',
-    'menu_item_parent' => $cal_nav_pseudo_id,
-    'db_id' => $year_nav_pseudo_id,
-    'current' => false,
-    'xfn'     => '',
-    'target'  => '',
-    'classes' => array( 'menu-item', 'menu-item-has-children' ),
-    'url' => site_url( '/veranstaltungen/' . $current_year )
-  );
+  lfm_add_year_month_items ($items, $today_dt, $year_nav_pseudo_id, $cal_nav_pseudo_id);
 
   /*
-  $items[] = (object) array(
-    'title' => $today_dt->format( 'F' ),
-    'ID' => 'nav_current_month',
-    'menu_item_parent' => $current_walk_id,
-    'db_id'   => $current_walk_id += 1,
-    'current' => false,
-    'target'  => '',
-    'url' => 'https://bog.us/me/too'
-  );
-   */
+  $next_year = new DateTime();
+  $year_nav_pseudo_id+= 13;
+  lfm_add_year_month_items ($items, $next_year, $year_nav_pseudo_id, $cal_nav_pseudo_id);
+  */
 
-  $current_pseudo_id = $year_nav_pseudo_id;
-  while ( $today_dt->format( 'Y' ) == $current_year ) {
-    $items[] = (object) array(
-      'title' => date_i18n( 'F', $today_dt->getTimestamp()),
-      'ID' => 'nav_current_month',
-      'menu_item_parent' => $year_nav_pseudo_id,
-      'db_id'   => $current_pseudo_id += 1,
-      'current' => false,
-      'xfn'     => '',
-      'target'  => '',
-      'url'     => site_url( '/veranstaltungen/' . $current_year . '/' . $today_dt->format( 'm' ) )
-    );
-    $today_dt->modify( '+1 month' );
-  }
   return $items;
 }
 add_filter( 'wp_nav_menu_objects', 'lfm_add_calendar_items', 10, 2 );
