4ee3ca943dbcf65bbddb3fa6727caa77da2972a4
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-05-14 09:14:26 +0200

    Stub for TinyMCE word limit warning (#6)

--- editor_ui/editor_ui.php
+++ editor_ui/editor_ui.php
@@ -48,4 +48,34 @@ function lfm_remove_caldera_form_link() {
   }
 }
 
+add_filter( 'admin_print_footer_scripts', 'lfm_add_tinymce_word_limits' );
+function lfm_add_tinymce_word_limits() {
+  global $post_type;
+  if ('Your-Post-Type-here' != $post_type) {
+    //return;
+  }
+  ?>
+  <script type="text/javascript">
+  window.onload = function () {
+    tinyMCE.editors[0].on('keyup', function(ed,e) {
+      var word_count_element = document.querySelectorAll('.word-count')[0];
+       var word_count = parseInt( word_count_element.textContent );
+       if(word_count > 3) {
+         document.getElementById('post-status-info').style.backgroundColor = '#ff0000';
+         var text_to_change = document.getElementById('wp-word-count').childNodes[0];
+         text_to_change.nodeValue = 'Wortanzahl (max 3): ';
+       } else {
+         document.getElementById('post-status-info').style.backgroundColor = '#f7f7f7';
+         var text_to_change = document.getElementById('wp-word-count').childNodes[0];
+         text_to_change.nodeValue = 'Wortanzahl: ';
+       }
+    });
+  }
+  </script>
+
+	<style type="text/css">
+	</style>
+  <?php
+}
+
 ?>
