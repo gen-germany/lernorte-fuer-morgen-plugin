252f3f17928376fd3477e4f3f783622f6a8d4dc2
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-05-15 10:29:15 +0200

    whitespace cleanup

--- editor_ui/editor_ui.php
+++ editor_ui/editor_ui.php
@@ -67,19 +67,20 @@ function lfm_add_tinymce_word_limits() {
     if (!tinyMCE) { return; }
 
     var word_limit = <?php echo $word_limit; ?>;
+
     tinyMCE.editors[0].on('keyup', function(ed,e) {
       var word_count_element = document.querySelectorAll('.word-count')[0];
-       var word_count = parseInt( word_count_element.textContent );
-       if(word_count > word_limit) {
-         document.getElementById('post-status-info').style.backgroundColor = '#ff3333';
-         var text_to_change = document.getElementById('wp-word-count').childNodes[0];
-         text_to_change.nodeValue = 'Wortanzahl (max ' + word_limit + '): ';
-       } else {
-         document.getElementById('post-status-info').style.backgroundColor = '#f7f7f7';
-         var text_to_change = document.getElementById('wp-word-count').childNodes[0];
-         text_to_change.nodeValue = 'Wortanzahl (max ' + word_limit + '): ';
-         //text_to_change.nodeValue = 'Wortanzahl: ';
-       }
+      var word_count = parseInt( word_count_element.textContent );
+      if(word_count > word_limit) {
+        document.getElementById('post-status-info').style.backgroundColor = '#ff3333';
+        var text_to_change = document.getElementById('wp-word-count').childNodes[0];
+        text_to_change.nodeValue = 'Wortanzahl (max ' + word_limit + '): ';
+      } else {
+        document.getElementById('post-status-info').style.backgroundColor = '#f7f7f7';
+        var text_to_change = document.getElementById('wp-word-count').childNodes[0];
+        text_to_change.nodeValue = 'Wortanzahl (max ' + word_limit + '): ';
+        //text_to_change.nodeValue = 'Wortanzahl: ';
+      }
     });
   }
   </script>
